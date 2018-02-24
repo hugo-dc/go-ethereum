37207a335c047e4b9065c6761dbbfbe222a59c38
cdetrio <cdetrio@gmail.com>
2018-02-24 15:03:23 -0500

    fix

--- core/state/database.go
+++ core/state/database.go
@@ -19,6 +19,7 @@ package state
 import (
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/trie"
