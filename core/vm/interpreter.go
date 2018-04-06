7eabcc8a88f7c468f0aeeab8d09740e1f0b7031e
cdetrio <cdetrio@gmail.com>
2018-04-06 17:08:05 -0400

    fix deps

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -21,6 +21,8 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/params"
 )
